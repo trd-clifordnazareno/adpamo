1929518f4124f6d2bb834f5c9464e1543794054a
trd-clifordnazareno <trd.clifordnazareno@gmail.com>
2020-11-23 17:12:22 +0800

    Update clients_model.php

--- models/clients_model.php
+++ models/clients_model.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "adpamo";
+$servername = "bitkvtrcb83u4zewwxlm-mysql.services.clever-cloud.com";
+        $username = "uoda79man3frv2ey";
+        $password = "7j6L6wOWNiGBz68Bp5TQ";
+        $dbname = "bitkvtrcb83u4zewwxlm";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
