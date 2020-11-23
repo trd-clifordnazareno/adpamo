69e70bcc2ba5e670ea9d33e1ee1e123065de2d2b
trd-clifordnazareno <trd.clifordnazareno@gmail.com>
2020-11-23 17:28:45 +0800

    Update clients.php

--- controller/clients.php
+++ controller/clients.php
@@ -1,5 +1,6 @@
 <?php
-///header("Content-Type: application/json; charset=UTF-8");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Origin: *");
 class Clients {
   // Properties
   public $client_id;
