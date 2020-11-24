27d0fbc3297f2e15a12bb6ddf933da19d507dc16
trd-clifordnazareno <trd.clifordnazareno@gmail.com>
2020-11-24 15:15:31 +0800

    Update index.php

--- index.php
+++ index.php
@@ -223,6 +223,7 @@ $scope.edit_clients_form = function(clients_id, clients_name, clients_address, c
                                       "&&type_of_measurement="+$type_of_measurement)
                               .then(function (response) {
                                   $scope.insert_successful = false;
+                      alert("You Ordered Successfully");
                           });
           
           
