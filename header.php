a120525715d7acf5ce8382dae8d6c9fad0f77a8d
trd-clifordnazareno <trd.clifordnazareno@gmail.com>
2020-12-02 13:26:49 +0800

    Update header.php

--- header.php
+++ header.php
@@ -44,7 +44,7 @@
     <ul class="navbar-nav ml-auto">
       <!-- Messages Dropdown Menu -->
       <li class="nav-item dropdown">
-        <--<a class="nav-link" data-toggle="dropdown" href="#">
+        <!--<a class="nav-link" data-toggle="dropdown" href="#">
           <i class="far fa-comments"></i>
           <span class="badge badge-danger navbar-badge">3</span>
         </a>-->
