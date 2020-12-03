faa0872d644a2fac79e2145d1af3c13f7eaaabdb
trd-clifordnazareno <trd.clifordnazareno@gmail.com>
2020-12-03 09:29:33 +0800

    Update header.php

--- header.php
+++ header.php
@@ -13,6 +13,12 @@
   <link rel="stylesheet" href="template/dist/css/adminlte.min.css">
   <!-- Google Font: Source Sans Pro -->
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+  
+  
+  
+  
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.css" rel="stylesheet">
+   <link href="https://cdn.datatables.net/1.10.22/css/dataTables.bootstrap4.min.css" rel="stylesheet">
 </head>
 <body class="sidebar-mini" style="height: auto;">
 <!-- Site wrapper -->
