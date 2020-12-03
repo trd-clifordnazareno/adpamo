9432edfd27e6c3dcdc21c30c7b77cef5dd7fe6a6
trd-clifordnazareno <trd.clifordnazareno@gmail.com>
2020-12-03 09:42:06 +0800

    Update header.php

--- header.php
+++ header.php
@@ -17,8 +17,8 @@
   
   
   
-  <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.css" rel="stylesheet">
-   <link href="https://cdn.datatables.net/1.10.22/css/dataTables.bootstrap4.min.css" rel="stylesheet">
+  <link href="https://cdn.datatables.net/1.10.22/css/jquery.dataTables.min.css" rel="stylesheet">
+   
 </head>
 <body class="sidebar-mini" style="height: auto;">
 <!-- Site wrapper -->
