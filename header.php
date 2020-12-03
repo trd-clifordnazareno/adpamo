2d5bbd626298a012794456d211bf75e1bd054576
trd-clifordnazareno <trd.clifordnazareno@gmail.com>
2020-12-03 09:18:41 +0800

    Update header.php

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <html style="height: auto;"><head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>Adpam</title>
+  <title>Adpamo</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
